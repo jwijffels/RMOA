a2978a22e9a00aecd5631a8875f42b1c726e9931
Jan Wijffels <jwijffels@bnosac.be>
2014-04-26 00:02:30 +0200

    Add more classifiers to the knownmodels: AdaHoeffdingOptionTree, ASHoeffdingTree, DecisionStump, HoeffdingAdaptiveTree, HoeffdingOptionTree, HoeffdingTree, LimAttHoeffdingTree, RandomHoeffdingTree, NaiveBayes

--- RMOA/pkg/R/knownmodels.R
+++ RMOA/pkg/R/knownmodels.R
@@ -1,9 +1,27 @@
 modelclass <- function(model){
-  knownmodels <- c("HoeffdingTree", "NaiveBayes")
+  knownmodels <- c("AdaHoeffdingOptionTree", "ASHoeffdingTree", "DecisionStump", "HoeffdingAdaptiveTree", "HoeffdingOptionTree",
+                   "HoeffdingTree", "LimAttHoeffdingTree", "RandomHoeffdingTree", "NaiveBayes")
   if(!model %in% knownmodels){
     stop(sprintf("%s not an implemented model", as.character(model)))
   }
   recoder(model, 
-          from = c("HoeffdingTree","NaiveBayes"),
-          to = c("moa/classifiers/trees/HoeffdingTree", "moa/classifiers/bayes/NaiveBayes"))
+          from = c("AdaHoeffdingOptionTree",
+                   "ASHoeffdingTree",
+                   "DecisionStump",
+                   "HoeffdingAdaptiveTree",
+                   "HoeffdingOptionTree",
+                   "HoeffdingTree",
+                   "LimAttHoeffdingTree", 
+                   "RandomHoeffdingTree",
+                   "NaiveBayes"),
+          to = c("moa/classifiers/trees/AdaHoeffdingOptionTree",
+                 "moa/classifiers/trees/ASHoeffdingTree",
+                 "moa/classifiers/trees/DecisionStump",
+                 "moa/classifiers/trees/HoeffdingAdaptiveTree",
+                 "moa/classifiers/trees/HoeffdingOptionTree",
+                 "moa/classifiers/trees/HoeffdingTree", 
+                 "moa/classifiers/trees/LimAttHoeffdingTree", 
+                 "moa/classifiers/trees/RandomHoeffdingTree", 
+                 "moa/classifiers/bayes/NaiveBayes"))
 }
+
