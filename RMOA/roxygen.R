9b2df79e0bbe8f1d7d960b269e51093cc091688f
Jan Wijffels <jwijffels@bnosac.be>
2014-05-25 15:55:47 +0200

    Move to latest version of roxygen2 and export regressors to namespace

--- RMOA/roxygen.R
+++ RMOA/roxygen.R
@@ -5,11 +5,7 @@ options(error=traceback)
 unlink( 'pkg/man', TRUE)
 
 setwd('pkg')
-roxygenize( '.'
-          , roxygen.dir='.'
-          , copy.package=FALSE
-          , unlink.target=TRUE
-		    )
+roxygenize('.')
 
 if (length(list.files('inst/doc')) == 0){
    unlink( 'inst/doc', TRUE)   
